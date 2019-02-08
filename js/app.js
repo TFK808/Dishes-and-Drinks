760ce7e21dc8df39929b53da11951fcf2a3b3a5b
Lauren Bullen <laurenbullen@Laurens-MacBook-Pro.local>
2019-02-07 18:31:52 -0800

    drinks work-ish

--- js/app.js
+++ js/app.js
@@ -95,8 +95,6 @@ $(document).on("click", ".liquor", function() {
 
 
     var liquor = $(this).attr("data-name");
-    // var wine = $("#wine").val().trim();
-    // var beer = 
     console.log(liquor);
 
     var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + liquor;
@@ -108,8 +106,28 @@ $(document).on("click", ".liquor", function() {
       })
       // After the data comes back from the API
       .then(function(response) {
-    //       console.log(response);
-    
+          console.log(response);
+        //store an array of results
+        var results = response.drinks;
+       
+        // loop through response    
+        for (let i = 0; i < 5; i++) {
+            // creating a div for the drinks
+            var drinkDiv = $("<div>");
+            // creating p for the name of the cockail
+            var p = $("<p>").text("Cocktail Name: " + results[i].drinks);
+            // creating an image tag for the drinks in that div
+            var drinkImage = $("<img>");
+            // add attributes to image
+            drinkImage.addClass("drink");
+            drinkImage.attr("src", results[i].strDrinkThumb);
+            // append image and name of drink
+            drinkDiv.append(p);
+            drinkDiv.append(drinkImage);
+
+            $("#inputResults").prepend(drinkDiv)
+        }
+    })
 
 
 // ******* Reset Button ******* //
@@ -121,7 +139,7 @@ $(document).ready(function() {
     });
 });
 
-      })
+//       })
     
 });
 
